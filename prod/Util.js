0c03f16ebd47184c00b586532abaa47a9bbd7591
Lucas Gerrits <lucasjgerrits@gmail.com>
2024-03-19 08:03:28 -0500

    Delete prod directory

--- prod/Util.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Util = void 0;
-class Util {
-    static async sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-}
-exports.Util = Util;
